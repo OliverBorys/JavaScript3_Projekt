8624b58901b3c386938c550ee3e79113e54f0eca
Oliver Borys <oliver.borys@gmail.com>
2025-04-11 08:38:39 +0200

    Added footer and easy header for navigation to the first routes I will set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'client';
+
+constructor(private http: HttpClient) {
+  this.http.get('/api/products').subscribe(tasks => {
+    console.log(tasks);
+  });
+}
 }
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
+
+  navItems = ['Shop', 'About', 'Contact'];
+
+  socialLinks = [
+    {
+      href: 'https://www.facebook.com',
+      label: 'Facebook',
+      color: '#0866ff',
+      icon: 'facebook',
+    },
+    {
+      href: 'https://www.instagram.com',
+      label: 'Instagram',
+      color: '#f601b8',
+      icon: 'instagram',
+    },
+    {
+      href: 'https://twitter.com',
+      label: 'Twitter',
+      color: '#1c9cea',
+      icon: 'twitter',
+    },
+    {
+      href: 'https://github.com/OliverBorys/JavaScript2_OliverBorys_VG',
+      label: 'GitHub',
+      color: 'black',
+      icon: 'github',
+    },
+    {
+      href: 'https://www.youtube.com/',
+      label: 'YouTube',
+      color: '#ff0033',
+      icon: 'youtube',
+    },
+  ];
+}
