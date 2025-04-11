76afc734260d8b4cc616cec5a777a168b401c5a2
Oliver Borys <oliver.borys@gmail.com>
2025-04-11 09:42:07 +0200

    Added category grid and about find us on home

--- src/app/components/about-find-us/about-find-us.component.ts
+++ src/app/components/about-find-us/about-find-us.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-about-find-us',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-find-us.component.html',
   styleUrl: './about-find-us.component.css'
 })
 export class AboutFindUsComponent {
+constructor(private router: Router) {}
 
+sections = [
+  {
+    name: 'About Us',
+    img: 'https://png.pngtree.com/background/20230516/original/pngtree-stock-image-of-a-clothing-rack-picture-image_2601916.jpg',
+    link: '/about'
+  },
+  {
+    name: 'Find Us',
+    img: 'https://png.pngtree.com/background/20221109/original/pngtree-city-map-gps-navigation-with-location-pin-markers-picture-image_1953527.jpg',
+    link: 'https://maps.app.goo.gl/JTQBjAJAU5h5yc6j9',
+    external: true,
+  }
+];
+
+handleClick(section: any): void {
+  if (section.external) {
+    window.open(section.link, '_blank');
+  } else {
+    this.router.navigate([section.link]);
+  }
+}
 }
--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
+import { CategoryGridComponent } from '../../components/category-grid/category-grid.component';
+import { AboutFindUsComponent } from '../../components/about-find-us/about-find-us.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSectionComponent],
+  imports: [HeroSectionComponent, CategoryGridComponent, AboutFindUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
