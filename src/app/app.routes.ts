d2245b7db54565a8c748ce1f8ac17ae27ff419d6
Oliver Borys <oliver.borys@gmail.com>
2025-04-11 16:49:30 +0200

    Added product-details page no function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './routes/home/home.component';
 import { ContactComponent } from './routes/contact/contact.component';
 import { AboutComponent } from './routes/about/about.component';
 import { ShopComponent } from './routes/shop/shop.component';
+import { ProductDetailsComponent } from './routes/product.details/product.details.component';
 
 export const routes: Routes = [
   { path: '',component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
   { path: 'shop', component: ShopComponent },
+  { path: 'product/:id', component: ProductDetailsComponent },
 
 ];
