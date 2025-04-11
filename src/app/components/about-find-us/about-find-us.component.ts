d02a746cb0539d8b68c29df7e8c1b70d67e40b94
Oliver Borys <oliver.borys@gmail.com>
2025-04-11 17:03:39 +0200

    Added shop link in header, changed to routerLink in about find us

--- src/app/components/about-find-us/about-find-us.component.ts
+++ src/app/components/about-find-us/about-find-us.component.ts
@@ -1,36 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-about-find-us',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './about-find-us.component.html',
   styleUrl: './about-find-us.component.css'
 })
 export class AboutFindUsComponent {
-constructor(private router: Router) {}
+  sections = [
+    {
+      name: 'About Us',
+      img: 'https://png.pngtree.com/background/20230516/original/pngtree-stock-image-of-a-clothing-rack-picture-image_2601916.jpg',
+      link: '/about',
+      external: false
+    },
+    {
+      name: 'Find Us',
+      img: 'https://png.pngtree.com/background/20221109/original/pngtree-city-map-gps-navigation-with-location-pin-markers-picture-image_1953527.jpg',
+      link: 'https://maps.app.goo.gl/JTQBjAJAU5h5yc6j9',
+      external: true
+    }
+  ];
 
-sections = [
-  {
-    name: 'About Us',
-    img: 'https://png.pngtree.com/background/20230516/original/pngtree-stock-image-of-a-clothing-rack-picture-image_2601916.jpg',
-    link: '/about'
-  },
-  {
-    name: 'Find Us',
-    img: 'https://png.pngtree.com/background/20221109/original/pngtree-city-map-gps-navigation-with-location-pin-markers-picture-image_1953527.jpg',
-    link: 'https://maps.app.goo.gl/JTQBjAJAU5h5yc6j9',
-    external: true,
+  openExternal(url: string): void {
+    window.open(url, '_blank');
   }
-];
-
-handleClick(section: any): void {
-  if (section.external) {
-    window.open(section.link, '_blank');
-  } else {
-    this.router.navigate([section.link]);
-  }
-}
 }
